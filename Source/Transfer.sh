0adcdd47482c3142ff35e5ab39deac0c7b70995d
LowerDimensions <LowerDimensions@gmail.com>
2023-10-13 05:30:52 -0400

    Update Transfer.sh

--- Source/Transfer.sh
+++ Source/Transfer.sh
@@ -3,8 +3,15 @@ staging="$1"
 stable="$2"
 paths="$3"
 
+echo 'Staging:'
+echo "${staging}"
+echo 'Stable:'
+echo "${stable}"
+echo 'Paths:'
+echo "${paths}"
 
-"${paths}" | xargs git checkout stable -- {}
+
+"${paths}" | xargs git checkout "${stable}" -- {}
 
 
 git status
